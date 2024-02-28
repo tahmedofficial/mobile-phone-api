3df6b0f0f2ce43fd64a75bc90b483fdb49a5590a
tahmedofficial <tanvirofcuo@gmail.com>
2024-02-28 23:24:37 +0600

    added phone details modal

--- scripts/phone.js
+++ scripts/phone.js
@@ -1,5 +1,5 @@
 
-const loadPhone = async (search) => {
+const loadPhone = async (search = "iphone") => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${search}`;
     const res = await fetch(url);
     const data = await res.json();
@@ -13,7 +13,6 @@ const displayPhones = phones => {
 
     // clear phone container card before adding new card
     phoneContainer.textContent = "";
-    // console.log(phones.length);
 
     // display show all button if there are more then 12 phones
     const showAllContainer = document.getElementById("show_all_container");
@@ -28,17 +27,18 @@ const displayPhones = phones => {
     phones = phones.slice(0, 12);
 
     phones.forEach(phone => {
+        // console.log(phone);
 
         const phoneCard = document.createElement("div");
         phoneCard.classList = `card bg-base-100 shadow-xl py-10`;
         phoneCard.innerHTML = `
             <figure><img src="${phone.image}" alt="Shoes" /></figure>
             <div class="card-body">
-                <h2 class="card-title">${phone.phone_name}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
-                </div>
+            <h2 class="card-title">${phone.brand}</h2>
+            <p>${phone.phone_name}</p>
+            <div class="card-actions justify-center mt-5">
+                <button onclick="handleShowDetails('${phone.slug}')" class="btn">Show Details</button>
+            </div>
             </div>
             `
         phoneContainer.appendChild(phoneCard);
@@ -46,6 +46,42 @@ const displayPhones = phones => {
     });
     // hide loading
     toggleLoading(false)
+}
+
+
+const handleShowDetails = async (id) => {
+    // load single phone data
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url)
+    const data = await res.json()
+    const phone = data.data;
+    showPhoneDeetails(phone)
+}
+
+const showPhoneDeetails = (phone) => {
+    console.log(phone);
+
+    const phoneName = document.getElementById("phone_name");
+    phoneName.innerText = phone.name;
+
+    const showDetailContainer = document.getElementById("show_detail_container");
+    showDetailContainer.innerHTML = `
+        <div class="flex justify-center"><img class="mt-5" src="${phone.image}" alt=""></div>
+        <p class="mt-5"><span>Storage : </span>${phone?.mainFeatures?.storage}</p>
+        <p class="mt-2"><span>Brand : </span>${phone?.brand}</p>
+        <p class="mt-2"><span>Chip Set : </span>${phone?.mainFeatures?.chipSet}</p>
+        <p class="mt-2"><span>Display Size : </span>${phone?.mainFeatures?.displaySize}</p>
+        <p class="mt-2"><span>Memory : </span>${phone?.mainFeatures?.memory}</p>
+        <p class="mt-2"><span>Bluetooth : </span>${phone?.others?.Bluetooth}</p>
+        <p class="mt-2"><span>GPS : </span>${phone?.others?.GPS}</p>
+        <p class="mt-2"><span>NFC : </span>${phone?.others?.NFC}</p>
+        <p class="mt-2"><span>Radio : </span>${phone?.others?.Radio}</p>
+        <p class="mt-2"><span>USB : </span>${phone?.others?.USB}</p>
+        <p class="mt-2"><span>WLAN : </span>${phone?.others?.WLAN}</p>
+    `
+
+    // show the modal
+    show_details_modal.showModal()
 }
 
 
@@ -66,3 +102,5 @@ const toggleLoading = (isLoading) => {
         loading.classList.add("hidden")
     }
 }
+
+loadPhone()
